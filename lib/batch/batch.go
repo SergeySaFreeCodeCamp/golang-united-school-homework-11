494e86593c154c5bfd8a1d532a6724ef8af4ad98
SergeySA <aledej@ya.ru>
2022-07-02 02:10:28 +0600

    lecture-11-solution

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -1,11 +1,22 @@
 package batch
 
 import (
+	"sync"
 	"time"
 )
 
 type user struct {
 	ID int64
+}
+
+var mu sync.Mutex
+
+func addUser(new_user user, res *[]user) {
+	// Lock the mutex before accessing res
+	mu.Lock()
+	// Unlock the mutex at the end of the function using a defer statement
+	defer mu.Unlock()
+	*res = append(*res, new_user)
 }
 
 func getOne(id int64) user {
@@ -14,5 +25,28 @@ func getOne(id int64) user {
 }
 
 func getBatch(n int64, pool int64) (res []user) {
-	return nil
+	res = make([]user, 0, n)
+
+	// channel - to know the amount of work
+	amountUsers := make(chan int, n)
+	for j := 0; j < int(n); j++ {
+		amountUsers <- j
+	}
+	close(amountUsers)
+
+	// Start pool workers
+	var wg sync.WaitGroup
+	for w := 0; w < int(pool); w++ {
+		wg.Add(1)
+		go func(amountUsers <-chan int, res *[]user) {
+			defer wg.Done()
+			for i := range amountUsers {
+				addUser(getOne(int64(i)), res)
+			}
+		}(amountUsers, &res)
+	}
+	// to ensure that the worker goroutines have finished
+	wg.Wait()
+
+	return res
 }
